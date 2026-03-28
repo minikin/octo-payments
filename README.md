fe79c40383b60121d9225528f9c2e3cc2208623f
minikin <warbles.lieu_04@icloud.com>
2026-03-28 14:41:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Payments Engine
+
+A streaming CSV payments engine that processes deposits, withdrawals, disputes, resolutions,
+and chargebacks, then outputs the final state of all client accounts.
---
+++ src/error.rs
@@ -0,0 +1,16 @@
+use core::error;
+
+use thiserror::Error;
+
+#[non_exhaustive]
+#[derive(Debug, Error)]
+pub enum PaymentEngineError {
+    #[error("CSV error: {0}")]
+    Csv(#[from] csv::Error),
+
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Missing amount for tx{0}")]
+    MissingAmount(u32),
+}
